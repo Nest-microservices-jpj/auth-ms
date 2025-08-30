f3d9cbe1c7ef96759e1df5b7f9670835af31d6ab
= <=>
2025-08-30 11:59:12 -0300

    auth implemented

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,123 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from 'generated/prisma';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { envs } from 'src/config';
+
+@Injectable()
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('AuthService');
+
+  constructor(
+    private readonly jwtService: JwtService
+  ) {
+    super();
+  }
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('MongoDb connected');
+  }
+
+  async signJwt(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = await this.user.create({
+        data: {
+          name,
+          email,
+          password: bcrypt.hashSync(password, 10),
+        },
+      });
+
+      const { password: _, ...rest } = newUser;
+
+      return {
+        user: rest,
+        token: await this.signJwt(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'Invalid credentials',
+        });
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+      if (!isPasswordValid) {
+        throw new RpcException({
+          status: 400,
+          message: 'Invalid credentials',
+        });
+      }
+
+      const { password: _, ...rest } = user;
+
+      return {
+        user: rest,
+        token: await this.signJwt(rest),
+      };
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+
+  async verifyToken(token: string) {
+    try {
+      const {sub, iat, exp, ...user} = await this.jwtService.verify(token, {
+        secret: envs.jwtSecret,
+      });
+      return {
+        user,
+        token: await this.signJwt(user),
+      }
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3010);
+  const logger = new Logger('OrdersMs-main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natServers,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen();
+  logger.log(`Auth Microservice running on port ${envs.port}`);
 }
 bootstrap();
